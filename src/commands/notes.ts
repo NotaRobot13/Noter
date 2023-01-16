bbaaf14226f331fc8a5e4b9229a7e66e46a8e997
NotaRobot13 <20willatss@kls.herts.sch.uk>
2023-01-16 16:32:44 +0000

    add roles

--- src/commands/notes.ts
+++ src/commands/notes.ts
@@ -47,29 +47,60 @@ module.exports = {
                         .setName('user')
                         .setDescription('The user to get the notes for.')
                         .setRequired(true)
-                )     
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('addrole')
+                .setDescription('Add a role to the list of roles that can use this command.')
+                .addRoleOption(option =>
+                    option
+                        .setName('role')
+                        .setDescription('The role to add.')
+                        .setRequired(true)
+                )
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('removerole')
+                .setDescription('Remove a role from the list of roles that can use this command.')
+                .addRoleOption(option =>
+                    option
+                        .setName('role')
+                        .setDescription('The role to remove.')
+                        .setRequired(true)
+                )
         ),
 
     async execute(interaction: ChatInputCommandInteraction<CacheType>) {
         await interaction.deferReply();
-        const notes = JSON.parse(fs.readFileSync('./notes.json').toString());
-        const userid = interaction.options.getUser('user').id;
+        const notes = JSON.parse(fs.readFileSync('./info.json').toString());
+        const userid = interaction.options.getUser('user')?.id;
+        const roles = JSON.parse(fs.readFileSync('./roles.json').toString());
+        // loop over the stored roles and check if the user has any of them
+        let hasRole = false;
+        for (const role of interaction.guild.roles.cache.map(role => role)) {
+            if (roles.includes(role.id)) hasRole = true;
+        }
+        if (interaction.guild.ownerId == interaction.user.id) hasRole = true;
+        if (!hasRole) return await interaction.editReply('You don\'t have permission to use this command.');
+        const role = interaction.options.getRole('role')?.id;
         switch (interaction.options.getSubcommand()) {
             case 'add':
                 const note = interaction.options.getString('note');
                 if (!notes[userid]) notes[userid] = { notes: [] };
                 notes[userid].notes.push(note);
-                fs.writeFileSync('./notes.json', JSON.stringify(notes));
+                fs.writeFileSync('./info.json', JSON.stringify(notes));
                 interaction.editReply('Added note.');
                 break;
             case 'remove':
                 const index = interaction.options.getNumber('index');
                 if (!notes[userid]) return await interaction.editReply("That user doesn't have any notes.");
                 if (!notes[userid].notes[index - 1]) return await interaction.editReply("That note doesn't exist.");
-                notes[userid].notes = notes[userid].notes.filter(function(item) {
+                notes[userid].notes = notes[userid].notes.filter(function (item) {
                     return notes[userid].notes.indexOf(item) !== index - 1;
                 })
-                fs.writeFileSync('./notes.json', JSON.stringify(notes));
+                fs.writeFileSync('./info.json', JSON.stringify(notes));
                 await interaction.editReply('Removed note.');
                 break;
             case 'get':
@@ -81,8 +112,22 @@ module.exports = {
                 });
                 await interaction.editReply(`\`\`\`${notesString}\`\`\``);
                 break;
+            case 'addrole':
+                if (!role) return await interaction.editReply('That role doesn\'t exist.');
+                if (roles.includes(role)) return await interaction.editReply('That role is already in the list.');
+                roles.push(role);
+                fs.writeFileSync('./roles.json', JSON.stringify(roles));
+                await interaction.editReply('Added role.');
+                break;
+            case 'removerole':
+                if (!role) return await interaction.editReply('That role doesn\'t exist.');
+                if (!roles.includes(role)) return await interaction.editReply('That role isn\'t in the list.');
+                roles.splice(roles.indexOf(role), 1);
+                fs.writeFileSync('./roles.json', JSON.stringify(roles));
+                await interaction.editReply('Removed role.');
+                break;
         }
 
-            
+
     },
 };
